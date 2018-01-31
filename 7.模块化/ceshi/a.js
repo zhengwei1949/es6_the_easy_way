507b47aef83f337d35851f02c08d68721be47a65
zhengwei <zhengwei1949@qq.com>
2018-01-31 20:50:29 +0800

    a

---
+++ 7.模块化/ceshi/a.js
@@ -0,0 +1,10 @@
+export let a = 1 
+export let b = 2
+export function c(m,n){
+    return m + n 
+}
+//只导出一个东西
+// export let a = 1;
+// export default let a = 1;
+// let a = 1;
+// export default a;
---
+++ 7.模块化/ceshi/main.js
@@ -0,0 +1,7 @@
+// import a from './a.js'
+// console.log(a)
+// console.log(c(2,3,))
+// import {a,b,c} from './a.js'
+// console.log(a,b,c)
+import * as obj from './a.js'
+console.log(obj)
